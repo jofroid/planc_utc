cd06d532e5e36b46e24641b43c798bca0674bca0
bsenechal <b_e_n@live.fr>
2014-06-07 21:14:12 +0200

    ajout info_profil

---
+++ site/app/actions/classes/Etudiant.class.php
@@ -0,0 +1,29 @@
+<?php
+Class Etudiant {
+
+	//Attributs
+	public $nom;
+	public $prenom;
+	public $sexe;
+	public $age;
+	public $login;
+
+	function __construct ($nom, $prenom, $sexe, $age, $login){
+		$this->nom = $nom;
+		$this->prenom = $prenom;
+		$this->sexe = $sexe;
+		$this->age = $age;
+		$this->login = $login;
+	}
+	
+	
+	function inserer () {
+		
+	}
+	
+	public static function getEtudiants(){
+		
+	}
+};
+
+?>
