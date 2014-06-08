caa67b160542d0bf8cfc23a4a12f20f356636862
bsenechal <b_e_n@live.fr>
2014-06-08 11:14:25 +0200

    correction bug

--- site/app/classes/Infos_profil.class.php
+++ site/app/classes/Infos_profil.class.php
@@ -15,11 +15,12 @@ class Infos_profil
 	}
 	
 	public function updateModifs() {
+
 		if (strlen($_POST['telephone']) == 10 && strlen($_POST['semestre']) < 10 && strlen($_POST['semestre']) >2){
 			$quartier = $this->db->selectOne("quartier", "nom = '". Atomik::escape($_POST['adresse'])."'");
 
-			$data = array('tel' =>  Atomik::escape(Atomik::escape($_POST['telephone'])), 'adresse' => Atomik::escape($quartier['id']), 'semestre' =>Atomik::escape($_POST['semestre']));
-		 	$this->db->update("infos_profil", $data);
+			$data = array('tel' =>  Atomik::escape(Atomik::escape($_POST['telephone'])), 'adresse' => Atomik::escape($quartier['id']), 'semestre' =>Atomik::escape($_POST['semestre']), 'orientation' =>Atomik::escape($_POST['orientation']));
+		 	$this->db->update("infos_profil", $data ,	"loginEtudiant='" . Atomik::get("session.login") . "';");
 			return true;
 		}
 		else 
