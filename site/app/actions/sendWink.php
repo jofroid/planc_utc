a510342531c57959ac3ada20b4dc2a36af529085
Pierre-Gilles Leymarie <pg.boss@hotmail.fr>
2014-06-08 05:04:21 +0200

    envoie wink avec vérif existence destinataire

--- site/app/actions/sendWink.php
+++ site/app/actions/sendWink.php
@@ -1,11 +1,25 @@
 <?php
 
 	require_once 'app/classes/Wink.class.php';
+	require_once 'app/classes/permission.class.php';
 	use \Plancutc\app\classes;
 
+	$permission = new Permission();
+	if($permission->isRegistered())
+	{
+		$login_user = $permission->getLogin();
+		$wink = new Wink();
+		if($permission->loginExist($_POST['loginDestinataire']))
+		{
+			$wink->sendWink(date("Y:m:d"),$login_user,$_POST['loginDestinataire']);
+		}
+		else
+		{
+			echo "erreur => login n'existe pas";
+		}
+	}
+	else
+	{	
+		
+	}
 	
-	$wink = new Wink();
-	$wink->sendWink(date("Y:m:d"),"pleymari","veroclar");
-
-	$result = $wink->getWinkUser("pleymari");
-	echo(json_encode($result));
